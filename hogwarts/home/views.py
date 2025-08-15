bca9cd2e435bbc8d595c00ad87731787c2f4a9f9
Sreeraj Dabbiru <sreerajscs916225@gmail.com>
2025-08-15 12:56:47 +0530

    Added home in views.py

--- hogwarts/home/views.py
+++ hogwarts/home/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+
+def home(request):
+    return render(request, 'index.html')
+
 
 # Create your views here.
